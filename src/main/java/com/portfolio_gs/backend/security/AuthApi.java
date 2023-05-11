1db0feb36852c6ef638ff2b188918aee9c3efe21
soyElSando <german.tito.sandoval@gmail.com>
2023-05-11 14:03:33 -0300

    V0.8 Security & JWT

---
+++ src/main/java/com/portfolio_gs/backend/security/AuthApi.java
@@ -0,0 +1,46 @@
+//package com.portfolio_gs.backend.security;
+//
+//import com.portfolio_gs.backend.model.User;
+//import com.portfolio_gs.backend.security.jwt.JwtTokenUtil;
+//import javax.validation.Valid;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController
+//@CrossOrigin(origins= "http://localhost:4200")
+//public class AuthApi {
+//
+//    @Autowired
+//    AuthenticationManager authManager;
+//
+//    @Autowired
+//    JwtTokenUtil jwtTokenUtil;
+//
+//    @PostMapping("/api/login")
+//    public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request) {
+//        try {
+//            Authentication authentication = authManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+//            );
+//
+//            User user = (User) authentication.getPrincipal();
+//            String accessToken = jwtTokenUtil.generateAccesToken(user);
+//            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
+//
+//            return ResponseEntity.ok().body(response);
+//        } catch (BadCredentialsException ex) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
+//    }
+//
+//}
---
+++ src/main/java/com/portfolio_gs/backend/security/AuthResponse.java
@@ -0,0 +1,21 @@
+//
+//package com.portfolio_gs.backend.security;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//public class AuthResponse {
+//   private String email;
+//   private String accessToken;  
+//
+//    public AuthResponse() {
+//    }
+//
+//    public AuthResponse(String email, String accessToken) {
+//        this.email = email;
+//        this.accessToken = accessToken;
+//    }
+//   
+//    
+//}
