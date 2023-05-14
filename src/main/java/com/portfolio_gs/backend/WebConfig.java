64dbd1e2b3531beea0e499a0a013ba26b36be65e
soyElSando <german.tito.sandoval@gmail.com>
2023-05-14 13:54:14 -0300

    CORS_3

--- src/main/java/com/portfolio_gs/backend/WebConfig.java
+++ src/main/java/com/portfolio_gs/backend/WebConfig.java
@@ -1,24 +1,24 @@
-package com.portfolio_gs.backend;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@EnableWebMvc
-
-@Configuration
-
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("http://localhost:4200");
-
-    }
-    
-}
+//package com.portfolio_gs.backend;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@EnableWebMvc
+//
+//@Configuration
+//
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//        registry.addMapping("/**");
+//
+//    }
+//    
+//}
 
 //En caso de querer cambiar cors cambiar aquì y en AuthApi
--- src/main/java/com/portfolio_gs/backend/controller/MailController.java
+++ src/main/java/com/portfolio_gs/backend/controller/MailController.java
@@ -4,7 +4,7 @@ package com.portfolio_gs.backend.controller;
 import com.portfolio_gs.backend.model.Mail;
 import com.portfolio_gs.backend.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Mail")
-//@CrossOrigin(origins= "http://localhost:4200")
+@CrossOrigin(origins= "http://localhost:4200")
 public class MailController {
  
     @Autowired
