8767cf3aef1c123e85911a6d5dd1d8af74d191ce
soyElSando <german.tito.sandoval@gmail.com>
2023-05-14 12:58:10 -0300

    CORS

--- src/main/java/com/portfolio_gs/backend/security/AuthApi.java
+++ src/main/java/com/portfolio_gs/backend/security/AuthApi.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins= "/**")
+@CrossOrigin(origins= "http://localhost:4200")
 public class AuthApi {
 
     @Autowired
